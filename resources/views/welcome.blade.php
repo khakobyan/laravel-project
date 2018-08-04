bb827ce3aac8f0cd3a6064c50b35bf65aa6ad931
Karen Hakobyan <karenhakobyan1994@gmail.com>
2018-08-04 15:23:21 +0400

    react auth part

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!-- <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
@@ -7,10 +7,10 @@
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
+     
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
+      
         <style>
             html, body {
                 background-color: #fff;
@@ -92,5 +92,33 @@
                 </div>
             </div>
         </div>
+    </body>
+</html>
+ -->
+
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
+        <style>
+               .title{
+                    margin-top: 230px;
+                    color: cadetblue;
+               } 
+        </style>
+      
+        
+    </head>
+    <body>
+        <div id="app">
+           
+        </div>
+        <script src="{{asset('js/app.js')}}" ></script>
     </body>
 </html>
