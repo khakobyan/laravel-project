0608eb0b66986524db929a20ebc7cf8f83b07eeb
Karen Hakobyan <karenhakobyan1994@gmail.com>
2018-08-05 03:15:56 +0400

    user_model

--- app/User.php
+++ app/User.php
@@ -19,7 +19,12 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'active', 'activation_token',
+        'uuid',
+        'name', 
+        'email', 
+        'password', 
+        'active', 
+        'activation_token',
     ];
 
     /**
