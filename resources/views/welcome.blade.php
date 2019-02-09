0f99f336d2e3dae1538b007b6c97ce0644398cf1
Karen Hakobyan <karenhakobyan1994@gmail.com>
2019-02-09 17:18:40 +0400

    not ended

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,96 +1,85 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-     
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-      
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                    <a href="{{ route('export') }}">Export</a>
-                </div>
-            </div>
-        </div>
-    </body>
+   <head>
+       <meta charset="utf-8">
+       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+       <meta name="viewport" content="width=device-width, initial-scale=1">
+       <title>Laravel S3</title>
+       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+       <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+       <style>
+           body, .card{
+               background: #ededed;
+           }
+       </style>
+   </head>
+   <body>
+       <div class="container">
+           <div class="row pt-5">
+               <div class="col-sm-12">
+                   @if ($errors->any())
+                       <div class="alert alert-danger">
+                           <button type="button" class="close" data-dismiss="alert">×</button>
+                           <ul>
+                               @foreach ($errors->all() as $error)
+                                   <li>{{ $error }}</li>
+                               @endforeach
+                           </ul>
+                       </div>
+                   @endif
+                   @if (Session::has('success'))
+                       <div class="alert alert-info">
+                           <button type="button" class="close" data-dismiss="alert">×</button>
+                           <p>{{ Session::get('success') }}</p>
+                       </div>
+                   @endif
+               </div>
+               <div class="col-sm-8">
+                   @if (count($images) > 0)
+                       <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                           <div class="carousel-inner">
+                               @foreach ($images as $image)
+                                   <div class="carousel-item {{ $loop->first ? 'active' : '' }}">
+                                       <img class="d-block w-100" src="{{ $image['src'] }}" alt="First slide">
+                                       <div class="carousel-caption">
+                                           <form action="{{ url('images/' . $image['name']) }}" method="POST">
+                                               {{ csrf_field() }}
+                                               {{ method_field('DELETE') }}
+                                               <button type="submit" class="btn btn-default">Remove</button>
+                                           </form>
+                                       </div>
+                                   </div>
+                               @endforeach
+                           </div>
+                           <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                               <span class="sr-only">Previous</span>
+                           </a>
+                           <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                               <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                               <span class="sr-only">Next</span>
+                           </a>
+                       </div>
+                   @else
+                       <p>Nothing found</p>
+                   @endif
+               </div>
+               <div class="col-sm-4">
+                   <div class="card border-0 text-center">
+                       <form action="{{ url('/images') }}" method="POST" enctype="multipart/form-data" class="form-horizontal">
+                           {{ csrf_field() }}
+                           <div class="form-group">
+                               <input type="file" name="image" id="image">
+                           </div>
+                           <div class="form-group">
+                               <button type="submit" class="btn btn-primary">Upload</button>
+                           </div>
+                       </form>
+                   </div>
+               </div>
+           </div>
+       </div>
+       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+   </body>
 </html>
